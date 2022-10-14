11fce59f7a04a228e0889e999069db4f9b1dd40f
sergey-krasnodar <serega.ru71@mail.ru>
2022-10-14 07:45:22 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function (){
   var mixer = mixitup('.portfolio__inner');
+  $('.slider__inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>'
+  });
 });
