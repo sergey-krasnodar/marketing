14e97d9ad3b1c9f62bf709454e22dbca4ec9309c
sergey-krasnodar <serega.ru71@mail.ru>
2022-10-14 18:27:58 +0300

    pricing

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function (){
   var mixer = mixitup('.portfolio__inner');
   $('.slider__inner').slick({
     dots: true,
-    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-    nextArrow: '<button type="button" class="slick-next">Next</button>'
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-right.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-left.svg" alt=""></button>'
   });
 });
