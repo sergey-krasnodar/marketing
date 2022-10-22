b518edf4ab6c4daf3f19a7dff0901367f792a1d5
sergey-krasnodar <serega.ru71@mail.ru>
2022-10-22 15:34:10 +0300

    scroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 $(function (){
-  var mixer = mixitup('.portfolio__inner');
+
+  $(".header__menu a, .address__link").on("click", function (event){
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $('.menu__btn, .header__menu-link').on('click', function (){
+    $('.header__inner').toggleClass('header__inner--active');
+  });
+  
   $('.slider__inner').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-right.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-left.svg" alt=""></button>'
   });
+  
+  var mixer = mixitup('.portfolio__inner');
 });
